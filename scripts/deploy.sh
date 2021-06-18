b29c8396d66dd79a67aacd6d940e7ba359c1f424
Qasim Abdullah <qasimabdullah404@gmail.com>
2021-06-18 16:21:17 +0500

    ...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,9 @@
 kubectl create namespace travis-example
 /usr/local/bin/kubectl create -f ./k8s/deployment.yaml
 /usr/local/bin/kubectl get deploy,po --namespace=travis-example
+# Make sure created pod is scheduled and running.
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n travis-example get pods -lapp=travis-example -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for travis-example deployment to be available"; kubectl get pods -n travis-example; done
+echo "ALL DONE!!!"
 
 
 
