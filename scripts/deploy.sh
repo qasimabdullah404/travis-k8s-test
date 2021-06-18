efe5cfb58bdb097b9b29da09f0d5418abd289761
Qasim Abdullah <qasimabdullah404@gmail.com>
2021-06-18 15:49:37 +0500

    ...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-docker context export k8s-test --kubeconfig
-cat k8s-test.kubeconfig
-docker run -v ${TRAVIS_BUILD_DIR}:/kube /usr/local/bin/kubectl --kubeconfig kubeconfig set image deployment/travispy travispy=qaxim/travis:py01
-
-
+/usr/local/bin/kubectl create -f ../k8s/deployment.yaml
+/usr/local/bin/kubectl get deploy,po
 
 
 
